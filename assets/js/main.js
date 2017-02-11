ebe51ec6358b2cc65bd84fb41d3538e2755e2f61
Rafael Mello Campanari <lab.rafamello@gmail.com>
2017-02-11 17:57:29 -0200

    Get an invite | Gathering more user info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
+
 // Segment
 !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="4.0.0";
 analytics.load("PeeC2J5foYare7wdLnPTFxws2uRSTvgE");
@@ -12,25 +14,39 @@ function App() {
   vm.newLead = newLead;
   vm.handleInvitationForm = handleInvitationForm;
 
+  init();
+
+  function init() {
+    axios.get('https://ipinfo.io/json')
+      .then(function (response) {
+        vm.geo = response.data;
+      });
+  }
+
   function handleInvitationForm(event) {
-    var form = event.target;
-    var email = form.querySelector('input[type="email"]').value;
-    sendZapier(email);
-    form.innerHTML = '<br><h2 class="title is-3">Thank you!</h2> <p>You will be contacted soon.<p>';
+    var form = event.target,
+        email = form.querySelector('input[type="email"]').value,
+        messageCt = form.querySelector('#form-message'),
+        formContent = form.querySelector('#form-content');
+
+    var data = {
+      email: email,
+      timestamp: new Date().toString(),
+      geolocation: vm.geo
+    };
+
+    sendZapier(data);
+
+    messageCt.removeAttribute('hidden');
+    formContent.setAttribute('hidden', 'true');
   }
 
-  function newLead(email) {
-    // sendZapier(email);
-
-    thankYouLead();
+  function newLead(data) {
+    sendZapier(data);
   }
 
-  function thankYouLead() {
-
-  }
-
-  function sendZapier(email) {
-      axios.post(ZAPIER_URL, 'email=' + email);
+  function sendZapier(data) {
+      axios.post(ZAPIER_URL, JSON.stringify(data));
   }
 
   return this;
